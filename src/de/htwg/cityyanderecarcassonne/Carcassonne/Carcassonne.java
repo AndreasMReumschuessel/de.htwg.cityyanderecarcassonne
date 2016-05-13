daa8a344d11e1902eed44d32abe5c8092687179d
hekrause <hekrause@htwg-konstanz.de>
2016-05-13 12:28:10 +0200

    Test commit

--- src/de/htwg/cityyanderecarcassonne/Carcassonne/Carcassonne.java
+++ src/de/htwg/cityyanderecarcassonne/Carcassonne/Carcassonne.java
@@ -3,7 +3,7 @@ package de.htwg.cityyanderecarcassonne.Carcassonne;
 public class Carcassonne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Das ist ein Test
 
 	}
 
