81e2a9eb84e1ea04ab81dde379220c24f9ab300f
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2016-06-10 10:40:29 +0200

    Rewritten Region to an Interface

---
+++ src/de/htwg/cityyanderecarcassonne/model/RegionInterface.java
@@ -0,0 +1,12 @@
+package de.htwg.cityyanderecarcassonne.model;
+
+public interface RegionInterface {
+	
+	void setPlayer(Player player);
+	
+	Player getPlayer();
+	
+	void setID(int id);
+	
+	int getID();
+}
