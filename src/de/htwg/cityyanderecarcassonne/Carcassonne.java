7e75599568c948aeadb98d07152ca5d788c9a77e
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2016-06-11 18:28:14 +0200

    Cleaned Carcassonne

--- src/de/htwg/cityyanderecarcassonne/Carcassonne.java
+++ src/de/htwg/cityyanderecarcassonne/Carcassonne.java
@@ -1,6 +1,10 @@
 package de.htwg.cityyanderecarcassonne;
 
 public final class Carcassonne {
+	
+	private Carcassonne() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
