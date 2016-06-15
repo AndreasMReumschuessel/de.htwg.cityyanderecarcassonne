1dd87e487936d878ef4008dfc72f1151247bcb72
hekrause <hekrause@htwg-konstanz.de>
2016-06-15 23:58:12 +0200

    Started with controller tests.

--- test/de/htwg/cityyanderecarcassonne/controller/impl/CarcassonneControllerTest.java
+++ test/de/htwg/cityyanderecarcassonne/controller/impl/CarcassonneControllerTest.java
@@ -1,5 +1,98 @@
 package de.htwg.cityyanderecarcassonne.controller.impl;
 
-public class CarcassonneControllerTest {
+import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.htwg.cityyanderecarcassonne.model.ICard;
+import de.htwg.cityyanderecarcassonne.model.IRegion;
+import de.htwg.cityyanderecarcassonne.model.Player;
+
+
+public class CarcassonneControllerTest {
+	
+	private CarcassonneController cController;
+	
+	@Before
+	public void setUp() throws Exception {
+		cController = new CarcassonneController(100, 110);
+	}
+	
+	@Test
+	public void setTownsquareTest() {
+
+	}
+	
+	@Test
+	public void getDimensionXTest() {
+		assertEquals(100, cController.getDimensionX());
+	}
+	
+	@Test
+	public void getDimensionYTest() {
+		assertEquals(110, cController.getDimensionY());
+	}
+	
+	@Test
+    public void setStatusTest() {
+
+    }
+    
+	@Test
+    public void getStatusMessageTest() {
+
+    }
+    
+	@Test
+    public void getTownsquareStringTest() {
+
+    }
+    
+	@Test
+    public void getStatusTest() {
+
+    }
+	
+	@Test
+	public void placeCardTest() {
+
+	}
+
+	@Test
+	public void takeCardTest() {
+
+	}
+
+	@Test
+	public void getPossibilitiesTest() {
+
+	}
+	
+	@Test
+	public void getRegionPossibilitiesTest(ICard card) {
+
+	}
+
+	@Test
+	public void placeMeepleTest(Player player,ICard card, IRegion region) {
+
+	}
+
+	@Test
+	public void changePlayerTest(Player player) {
+
+	}
+	
+	@Test
+	public void finishRoundTest()	{
+
+	}
+
+	@Test
+	public void rotateCardTest(String direction) {
+
+	}
 }
