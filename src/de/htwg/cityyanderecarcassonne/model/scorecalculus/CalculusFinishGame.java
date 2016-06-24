aef0e329c8e8cab5e71f185017f281ad80f236f8
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2016-06-24 16:51:22 +0200

    No errors at CalculusFinischGame

--- src/de/htwg/cityyanderecarcassonne/model/scorecalculus/CalculusFinishGame.java
+++ src/de/htwg/cityyanderecarcassonne/model/scorecalculus/CalculusFinishGame.java
@@ -1,6 +1,9 @@
 package de.htwg.cityyanderecarcassonne.model.scorecalculus;
 
+import de.htwg.cityyanderecarcassonne.model.ICard;
+import de.htwg.cityyanderecarcassonne.model.IRegion;
 import de.htwg.cityyanderecarcassonne.model.ITownsquare;
+import de.htwg.cityyanderecarcassonne.model.Player;
 
 public class CalculusFinishGame extends ScoreCalculus {
 
@@ -12,6 +15,17 @@ public class CalculusFinishGame extends ScoreCalculus {
 	public void refreshScore() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	protected void assignSchoolPoints(ICard card, int points) {
+		IRegion region = card.getCenterMiddle();
+		Player player = region.getPlayer();
+		
+		int oldScore = player.getScore();
+		player.setScore(oldScore + points);
+		region.setPlayer(null);
+		player.addMeeple();		
 	}
 
 }
