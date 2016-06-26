e7b07d1863aebe6f0f7f94cd3f33ab864864f61d
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2016-06-27 00:04:13 +0200

    GUI experiments

--- src/de/htwg/cityyanderecarcassonne/Carcassonne.java
+++ src/de/htwg/cityyanderecarcassonne/Carcassonne.java
@@ -3,6 +3,7 @@ package de.htwg.cityyanderecarcassonne;
 import java.util.Scanner;
 
 import de.htwg.cityyanderecarcassonne.controller.impl.CarcassonneController;
+import de.htwg.cityyanderecarcassonne.view.guitwo.GraphicalUI;
 import de.htwg.cityyanderecarcassonne.view.tui.TextUI;
 
 public final class Carcassonne {
@@ -10,6 +11,8 @@ public final class Carcassonne {
 	private static CarcassonneController controller;
 	private static TextUI tui;
 	private static Scanner in;
+	
+	private static GraphicalUI gui;
 	
 	private Carcassonne() {
 		
@@ -20,6 +23,8 @@ public final class Carcassonne {
 		controller = new CarcassonneController(150, 150);
 		tui = new TextUI(controller);
 		tui.printTUI();
+		
+		gui = new GraphicalUI(controller);
 		
 		boolean continu = true;
         in = new Scanner(System.in);
