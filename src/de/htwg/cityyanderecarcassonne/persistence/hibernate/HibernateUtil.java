d36e3f88fdc239603a2f1036de4ecddb1bdb172e
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2018-06-22 22:36:30 +0200

    Refactored HibernateUtil.java

--- src/de/htwg/cityyanderecarcassonne/persistence/hibernate/HibernateUtil.java
+++ src/de/htwg/cityyanderecarcassonne/persistence/hibernate/HibernateUtil.java
@@ -5,14 +5,11 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    static {
+    private static SessionFactory buildSessionFactory() {
         try {
-            Configuration config = new Configuration().configure("/hibernate.cfg.xml");
-            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(config.getProperties());
-            sessionFactory = config.buildSessionFactory(builder.build());
-            //sessionFactory = new Configuration().configure("/de/htwg/cityyanderecarcassonne/persistence/hibernate/hibernate.cfg.xml").buildSessionFactory();
+            return new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
         } catch (Throwable ex) {
             System.err.println("Failed to create sessionFactory object: " + ex);
             throw new ExceptionInInitializerError(ex);
