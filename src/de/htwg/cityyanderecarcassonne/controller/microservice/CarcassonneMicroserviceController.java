28a0c57695954de4fd3e1788b7cee610e3efefd6
Andreas M. Reumschüssel <andreas@reumschuessel.de>
2018-06-25 22:30:34 +0200

    Completed routes

--- src/de/htwg/cityyanderecarcassonne/controller/microservice/CarcassonneMicroserviceController.java
+++ src/de/htwg/cityyanderecarcassonne/controller/microservice/CarcassonneMicroserviceController.java
@@ -7,21 +7,29 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.PathMatchers;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import de.htwg.cityyanderecarcassonne.controller.ICarcassonneController;
+import de.htwg.cityyanderecarcassonne.model.IPlayer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
 public class CarcassonneMicroserviceController extends AllDirectives {
 
-    private ICarcassonneController controller;
-
     private final static String HOST_IP   = "localhost";
     private final static int    HOST_PORT = 8080;
+
+    private ICarcassonneController controller;
+
+    private Map<IPlayer, String> playerIdMap = new HashMap<>();
+    private String lastCardPosition;
 
     public CarcassonneMicroserviceController(ICarcassonneController controller) {
         this.controller = controller;
@@ -49,9 +57,87 @@ public class CarcassonneMicroserviceController extends AllDirectives {
     }
 
     private Route createRoute() {
-        return route(
-                pathPrefix("hello", () ->
-                    get(() ->
-                        complete("<h1>Say hello to akka-http</h1>"))));
+        return pathPrefix("cyc", () ->
+                        route(
+                                path("gamestatus", () ->
+                                        complete(controller.getStatus().toString())
+                                ),
+                                pathPrefix("addplayer", () ->
+                                        path(PathMatchers.remaining(), name ->
+                                                addPlayer(name)
+                                        )
+                                ),
+                                path("creategame", () -> {
+                                    controller.create();
+                                    controller.startRound();
+                                    return complete(StatusCodes.OK);
+                                }),
+                                path("currentplayer", () ->
+                                        complete("currentplayer")
+                                ),
+                                path("allplayers", () ->
+                                        complete("allplayers")
+                                ),
+                                path("currentcard", () ->
+                                        complete("currentcard")
+                                ),
+                                pathPrefix("rotatecard", () ->
+                                        path(PathMatchers.remaining(), direction ->
+                                                rotateCard(direction)
+                                        )
+                                ),
+                                path("cardcount", () ->
+                                        complete("cardcount")
+                                ),
+                                path("cardposslist", () ->
+                                        complete("cardposslist")
+                                ),
+                                pathPrefix("placecard", () ->
+                                        pathPrefix(PathMatchers.remaining(), selector ->
+                                                path(PathMatchers.remaining(), position ->
+                                                        complete(selector + ":" + position)
+                                                )
+                                        )
+                                ),
+                                path("meepleposslist", () ->
+                                        complete("meepleposslist")
+                                ),
+                                pathPrefix("placemeeple", () ->
+                                        pathPrefix(PathMatchers.segment(), selector ->
+                                                path(PathMatchers.remaining(), position ->
+                                                        complete(selector + ":" + position)
+                                                )
+                                        )
+                                ),
+                                path("gettownsquare", () ->
+                                        complete("gettownsquare")
+                                ),
+                                path("finnishround", () ->
+                                        complete("finnishround")
+                                )
+                            )
+        );
+    }
+
+    private Route addPlayer(String name) {
+        controller.addPlayer(name);
+
+        IPlayer controllerPlayer = controller.getPlayers().get(controller.getPlayers().size() - 1);
+
+        String pid = "pid_" + playerIdMap.size();
+        playerIdMap.put(controllerPlayer, pid);
+
+        return complete("kay lah");
+    }
+
+    private Route rotateCard(String direction) {
+        if ("left".equals(direction)) {
+            controller.rotateCardLeft();
+        } else if ("right".equals(direction)) {
+            controller.rotateCardRight();
+        } else {
+            return complete(StatusCodes.BAD_REQUEST, "Unknown direction. Only \"left\" and \"right\" possible.");
+        }
+        return complete("getCurrentCard()"); //TODO
     }
 }
